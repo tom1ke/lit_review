50d2fe9920d7f089e24e90bf22f49d969ba645dc
Thomas Roussel <thomas.roussel@ik.me>
2022-04-20 11:57:52 +0200

    settings variables, admin models

--- lit_review/lit_review/settings.py
+++ lit_review/lit_review/settings.py
@@ -125,3 +125,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'authentication.User'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.joinpath('media/')
--- lit_review/reviews/admin.py
+++ lit_review/reviews/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from reviews.models import Ticket, Review
 
-# Register your models here.
+admin.site.register(Ticket)
+admin.site.register(Review)
