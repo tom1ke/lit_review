cc9731a832885afabbbef796206f97737dd91a01
Thomas Roussel <thomas.roussel@ik.me>
2022-05-02 16:45:15 +0200

    removed help text from signup form

--- lit_review/authentication/forms.py
+++ lit_review/authentication/forms.py
@@ -1,8 +1,19 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 
 class SignupForm(UserCreationForm):
+    password1 = forms.CharField(label='Mot de passe',
+                                widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirmer mot de passe',
+                                widget=forms.PasswordInput)
+
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
-        fields = ('username', 'email')
+        fields = ('username', 'password1', 'password2')
+        help_texts = {
+            'username': None,
+            'password1': None,
+            'password2': None,
+        }
