403633b8c8aa5855094906293adbbb4240169efc
Thomas Roussel <thomas.roussel@ik.me>
2022-05-05 17:38:44 +0200

    content display & time format

--- lit_review/reviews/templatetags/reviews_extras.py
+++ lit_review/reviews/templatetags/reviews_extras.py
@@ -1,5 +1,8 @@
+import locale
 from django import template
 from django.utils import timezone
+
+locale.setlocale(locale.LC_TIME, "fr_FR")
 
 MINUTE = 60
 HOUR = 60 * MINUTE
@@ -17,10 +20,10 @@ def model_type(value):
 def time_created_display(time_created):
     seconds_ago = (timezone.now() - time_created).total_seconds()
     if seconds_ago <= HOUR:
-        return f'il y a {int(seconds_ago // MINUTE)} minutes.'
+        return f'il y a {int(seconds_ago // MINUTE)} minutes'
     elif seconds_ago <= DAY:
-        return f'il y a {int(seconds_ago // HOUR)} heures.'
-    return f'le {time_created.strftime("%d %b %y à %Hh%M")}'
+        return f'il y a {int(seconds_ago // HOUR)} heures'
+    return f'{time_created.strftime("%H:%M, %d %B %y")}'
 
 
 @register.simple_tag(takes_context=True)
